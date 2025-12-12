126389e56834ea48204af09abd5b3cd6ea66e986
Saendhor <comix143.mariorossi@gmail.com>
2025-12-12 15:41:08 +0100

    feat: added gitignore and saelib.h unique file

---
+++ saelib.h
@@ -0,0 +1,11 @@
+#ifndef SAELIB_H
+#define SAELIB_H
+
+/*
+*   Here should go all the ".h" files inside of the repository so that you may include all.
+*   You can still add headers separately
+*/
+
+// #include "sorting/*"
+
+#endif
