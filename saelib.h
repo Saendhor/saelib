4ac15ecbf8c020576abeea0fc76e3252ae46125a
Saendhor <comix143.mariorossi@gmail.com>
2026-04-13 16:45:26 +0200

    chore: updated main header file

--- saelib.h
+++ saelib.h
@@ -9,12 +9,18 @@
 //INCLUDE FOR MISC
 #include "misc/array_findmax.h"
 #include "misc/array_findmin.h"
-#include "misc/array_print.h"
+#include "misc/array_printf.h"
 #include "misc/swap.h"
 
 //INCLUDE FOR SORTING
 #include "sorting/bubblesort/bubblesort.h"
+#include "sorting/bucketsort/bucketsort.h"
+#include "sorting/countingsort/countingsort.h"
+#include "sorting/heapsort/heapsort.h"
 #include "sorting/insertionsort/insertionsort.h"
+#include "sorting/mergesort/mergesort.h"
+#include "sorting/quicksort/quicksort.h"
+#include "sorting/radixsort/radixsort.h"
 #include "sorting/selectionsort/selectionsort.h"
 
 #endif
