620e86ce62d9f89d08a30f46fb8c354bbeea23e1
Saendhor <comix143.mariorossi@gmail.com>
2026-04-13 15:37:26 +0200

    fix: removed unused code

--- sorting/heapsort/heapsort.h
+++ sorting/heapsort/heapsort.h
@@ -17,19 +17,3 @@ int heapsort(double input[], int size) {
 }
 
 #endif
-
-/*
-int heapsort(double input[], int size) {
-    heap_t heap = createheap_i(input, size);
-    heap.heap_size = size; // Assicurati che la dimensione sia corretta
-    maxheap_build(heap);
-    
-    // Si parte dall'ultimo elemento (size - 1) fino al secondo (1)
-    for (int i = size - 1; i > 0; i--) {
-        swap(&input[0], &input[i]);
-        heap.heap_size--; // Riduciamo l'heap virtuale
-        maxheap_heapify(heap, 0); // Ripristiniamo sempre dalla radice (0)
-    }
-    return 0;
-}
-    */
